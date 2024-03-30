3550583bf097bd17fe68e068b33d5e99dc74520f
hank.zheng <uinika@outlook.com>
2024-03-30 17:44:28 +0800

    Update example code.

--- Examples/11-ADC/Sources/main.c
+++ Examples/11-ADC/Sources/main.c
@@ -6,29 +6,19 @@
 #include "../Drivers/ADC/ADC.h"
 #include "../Drivers/USART/USART.h"
 
-unsigned int UINIO_ADC_Filter(unsigned int number) {
-  int index = 0;
-  unsigned int value = 0;
-
-  for(index = 0; index< number; index++) {
-    value = value + UINIO_ADC_Value(ADC_CHANNEL_11);
-    delay_1ms(1);
-  }
-
-  value = value / number;
-  return value;
-}
-
 int main(void) {
-  systick_config();
-  UINIO_USART_GPIO_Config(9600U);
-  UINIO_ADC_Config();
+  systick_config();               // 初始化系统滴答定时器
+  UINIO_USART_GPIO_Config(9600U); // 配置 USART0，并将波特率设置为 9600
+  UINIO_ADC_Config();             // 配置 ADC 外设
 
-  uint16_t value = 0;
+  uint16_t Voltage = 0;           // ADC 采集到的原始值（0 ~ 4095）
 
   while(1) {
-    value = UINIO_ADC_Filter(50);
-    printf("ADC_Value = %f\n",(( value / 4095.0 ) * 3.3) );
-    delay_1ms(500);
+    Voltage = UINIO_ADC_Value(ADC_CHANNEL_11);
+
+    /* 通过串口打印出实际电压值 */
+    printf("ADC_Value = %f\n",(( Voltage / 4095.0 ) * 3.3) ); // 接入 3.3V 采集的 ADC 值为 4095，接入 GND 采集的 ADC 值为 0
+
+    delay_1ms(1000); // 延时 1 秒
   }
 }
